cec615b08b559ef029709ccc39bfbbf4e1aa1922
Manish <mkghm12@github.com>
2022-04-22 02:11:28 +0530

    added routes

---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrownLogo } from '../../../assets/crown.svg';
+import './navigation.styles.scss';
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link to='/' className="logo-container">
+                    <CrownLogo className="logo" />
+                </Link>
+                <div className="nav-link-container">
+                    <Link to='/shop' className="nav-link">
+                        <h1>Shop</h1>
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
